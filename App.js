34d18dd26800540e8da6b9ab91762a784cb268fd
Adhy <adhywiranatap@gmail.com>
2017-05-16 09:30:33 +0700

    updated App

--- App.js
+++ App.js
@@ -1,30 +1,63 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 import { Provider, connect } from 'react-redux';
-import {Scene, Router} from 'react-native-router-flux';
+import { Scene, Router } from 'react-native-router-flux';
 
 import store from './src/store/configureStore';
-import HomeContainer from './src/containers/HomeContainer';
-import AuthContainer from './src/containers/AuthContainer';
-import ChatContainer from './src/containers/ChatContainer';
-import ProductContainer from './src/containers/ProductContainer';
-import CartContainer from './src/containers/CartContainer';
-import AnalyticsContainer from './src/containers/AnalyticsContainer';
+import {
+  HomeContainer,
+  AuthContainer,
+  ChatContainer,
+  ProductContainer,
+  CartContainer,
+  AnalyticsContainer,
+} from './src/containers';
+
+const styles = {
+  rootNavbar: {
+    backgroundColor: '#FFFFFF',
+    borderBottomColor: 'transparent',
+    elevation: 2,
+  },
+  homeNavbar: {
+    backgroundColor: 'transparent',
+    borderBottomColor: 'transparent',
+    elevation: 0,
+  },
+};
+
+const SceneWithoutNavbar = props => (
+  <Scene {...props} />
+);
+
+const SceneMenu = props => (
+  <Scene
+    {...props}
+    titleStyle={{ fontWeight: 'bold', color: '#FFFFFF' }}
+    navigationBarStyle={styles.homeNavbar}
+    hideNavBar={false}
+    renderBackButton={() => (null)}
+  />
+);
+
+const SceneWithNavbar = props => (
+  <Scene {...props} titleStyle={{ fontWeight: 'bold' }} hideNavBar={false} />
+);
 
 const MainRouter = () => (
   <Router>
-    <Scene key='root' navigationBarStyle={{backgroundColor: '#FFFFFF', borderBottomColor: 'transparent', elevation: 2 }}>
-      <Scene key='login' component={AuthContainer} title="login" hideNavBar={true} />
-      <Scene key='home' component={HomeContainer} title="BELLO" titleStyle={{ fontWeight: 'bold', color: '#FFFFFF' }} navigationBarStyle={{ backgroundColor: 'transparent', borderBottomColor: 'transparent', elevation: 0 }} hideNavBar={false} renderBackButton={() => (null)} />
-      <Scene key='chat' component={ChatContainer} title="BELLO" titleStyle={{ fontWeight: 'bold' }} hideNavBar={false} />
-      <Scene key='product' component={ProductContainer} title="PRODUCT" titleStyle={{ fontWeight: 'bold' }} hideNavBar={false} />
-      <Scene key='cart' component={CartContainer} title="CART" titleStyle={{ fontWeight: 'bold' }} hideNavBar={false} />
-      <Scene key='analytics' component={AnalyticsContainer} title="ANALISA" titleStyle={{ fontWeight: 'bold' }} hideNavBar={false} />
+    <Scene key="root" navigationBarStyle={styles.rootNavbar}>
+      <SceneWithoutNavbar key="login" component={AuthContainer} title="login" hideNavBar />
+      <SceneMenu key="home" component={HomeContainer} title="BELLO" hideNavBar={false}/>
+      <SceneWithNavbar key="chat" component={ChatContainer} title="Bello" />
+      <SceneWithNavbar key="chat" component={ChatContainer} title="BELLO" />
+      <SceneWithNavbar key="product" component={ProductContainer} title="PRODUCT" />
+      <SceneWithNavbar key="cart" component={CartContainer} title="CART" />
+      <SceneWithNavbar key="analytics" component={AnalyticsContainer} title="ANALISA" />
     </Scene>
   </Router>
 );
 
-const mapStateToProps = state =>  ({ scene: state.scene });
+const mapStateToProps = state => ({ scene: state.scene });
 
 const ConnectedRouter = connect(mapStateToProps, null)(MainRouter);
 

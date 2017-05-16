9cbc17a4b47b8c8a8a51dea134fceeac3f1bdd6d
Adhy <adhywiranatap@gmail.com>
2017-05-16 08:44:11 +0700

    added containers index

---
+++ src/containers/index.js
@@ -0,0 +1,8 @@
+import HomeContainer from './HomeContainer';
+import AuthContainer from './AuthContainer';
+import ChatContainer from './ChatContainer';
+import ProductContainer from './ProductContainer';
+import CartContainer from './CartContainer';
+import AnalyticsContainer from './AnalyticsContainer';
+
+export { HomeContainer, AuthContainer, ChatContainer, ProductContainer, CartContainer, AnalyticsContainer };

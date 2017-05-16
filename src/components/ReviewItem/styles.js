affcd151136b35d37f8fbe03e31573c380e79ce1
Adhy <adhywiranatap@gmail.com>
2017-05-16 10:08:23 +0700

    refactored review item, removed glamorous package

---
+++ src/components/ReviewItem/styles.js
@@ -0,0 +1,44 @@
+export default {
+  card: {
+    margin: 5,
+    marginTop: 10,
+    backgroundColor: '#FEFEFE',
+    borderRadius: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    flex: 1,
+    elevation: 2,
+    flexDirection: 'row',
+    borderColor: 'rgba(0, 0, 0, 0.05)',
+    borderWidth: 0.5,
+  },
+  cardImage: {
+    flex: 0.3,
+  },
+  cardContent: {
+    flex: 0.7,
+    padding: 10,
+  },
+  image: {
+    width: '100%',
+    resizeMode: 'contain',
+    borderRadius: 5,
+    alignSelf: 'flex-start',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 14,
+  },
+  price: {
+    fontWeight: 'bold',
+    fontSize: 14,
+    color: '#EB9532',
+  },
+  seller: {
+    fontSize: 14,
+    color: '#26A65B',
+  },
+  description: {
+    color: '#666666',
+  },
+};

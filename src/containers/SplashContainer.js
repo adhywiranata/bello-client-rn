268649bfcc57e94d45c1dad1d755503490842f46
Adhy <adhywiranatap@gmail.com>
2017-05-26 21:27:54 +0700

    update splash container linting errors

--- src/containers/SplashContainer.js
+++ src/containers/SplashContainer.js
@@ -1,49 +1,43 @@
 import React from 'react';
-import { AsyncStorage, Text, View, Image, TouchableOpacity } from 'react-native';
+import { AsyncStorage, View, Image } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
-import { connect } from 'react-redux'
-import { saveUserdata } from '../actions/userdata'
+import { connect } from 'react-redux';
+import { saveUserdata } from '../actions/userdata';
 
 import belloIcon from '../images/bello.png';
 
-
 class SplashContainer extends React.Component {
   componentDidMount = () => {
-    this.loadLoggedUserData().done()
+    this.loadLoggedUserData().done();
   }
 
   loadLoggedUserData = async () => {
     try {
-      const value = await AsyncStorage.getItem('@Bello:user')
-      if (value !== null){
-        const data = JSON.parse(value)
-        this.props.saveUserdata(data)
-        Actions.home()
-      }
-      else{
-        Actions.login()
+      const value = await AsyncStorage.getItem('@Bello:user');
+      if (value !== null) {
+        const data = JSON.parse(value);
+        this.props.saveUserdata(data);
+        Actions.home();
+      } else {
+        Actions.login();
       }
     } catch (error) {
-      alert('Error Retrieving Data : ' + error)
+      alert(`Error Retrieving Data : ${error}`);
     }
   }
-
 
   render() {
     return (
       <View>
-        <Image source={belloIcon} style={{ width: 200, height: 200}} />
+        <Image source={belloIcon} style={{ width: 200, height: 200 }} />
       </View>
     );
   }
 }
 
 const mapDispatchToProps = dispatch => ({
-  saveUserdata: (data) => dispatch(saveUserdata(data))
+  saveUserdata: data => dispatch(saveUserdata(data)),
 });
 
-const mapStateToProps = state => ({
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(SplashContainer)
+export default connect(null, mapDispatchToProps)(SplashContainer);
